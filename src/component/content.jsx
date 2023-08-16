c71c0bede750aab2bac4e58bb9fd3d870358f44a
Ahmad <farrel.ahmad.zaidan@gmail.com>
2023-08-17 06:25:34 +0700

    change content

--- src/component/content.jsx
+++ src/component/content.jsx
@@ -1,7 +1,7 @@
 function content() {
     return (
         <>
-            <section className="bg-gray-100  text-white">
+            <section className="bg-gray-100 text-white">
                 <div
                     className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
                 >
