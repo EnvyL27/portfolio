95c5df1dfc52328be6b660f12835b0695946b0d8
Ahmad <farrel.ahmad.zaidan@gmail.com>
2023-08-20 23:29:11 +0700

    add porto

--- src/component/portfolio.jsx
+++ src/component/portfolio.jsx
@@ -1,3 +1,9 @@
+import Sv from './../assets/solvevisual.png'
+import Tx from './../assets/taxi.png'
+import Us from './../assets/usfootwear.png'
+import Ms from './../assets/maissy.png'
+import Ab from './../assets/absensi.png'
+
 function portfolio() {
   return (
     <>
@@ -6,7 +12,7 @@ function portfolio() {
     <div class="flex justify-center items-center text-gray-800">
       <h2 class="text-3xl font-bold sm:text-4xl">Creative Endeavors</h2>
     </div>
-    <div class="flex justify-center items-center">
+    <div class="flex justify-center items-center text-center">
     <p class="mt-4 text-gray-500">
         Welcome to my "Creative Endeavors". Within this portfolio, you'll discover a collection of meticulously crafted web projects that embody the essence of modern online experiences.
       </p>
@@ -14,7 +20,77 @@ function portfolio() {
     <div
       class="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3"
     >
-      <div class="flex items-start gap-4">
+      <div class=" items-start gap-4 class">
+         <img src={Sv} alt=""  />
+
+        <div>
+          <h2 class="text-lg font-bold text-gray-800">Solvevisual.</h2>
+
+          <p class="mt-1 text-sm text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            cumque tempore est ab possimus quisquam reiciendis tempora animi!
+            Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      <div class=" items-start gap-4">
+      <img src={Us} alt=""  />
+
+        <div>
+          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            cumque tempore est ab possimus quisquam reiciendis tempora animi!
+            Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      <div class=" items-start gap-4">
+      <img src={Tx} alt=""  />
+
+        <div>
+          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            cumque tempore est ab possimus quisquam reiciendis tempora animi!
+            Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      <div class=" items-start gap-4">
+      <img src={Ms} alt=""  />
+
+        <div>
+          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            cumque tempore est ab possimus quisquam reiciendis tempora animi!
+            Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      <div class=" items-start gap-4">
+      <img src={Ab} alt=""  />
+
+        <div>
+          <h2 class="text-lg font-bold text-gray-800">Absensi.</h2>
+
+          <p class="mt-1 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            cumque tempore est ab possimus quisquam reiciendis tempora animi!
+            Quaerat, saepe?
+          </p>
+        </div>
+      </div>
+
+      {/* <div class="flex items-start gap-4">
         <span class="shrink-0 rounded-lg bg-gray-800 p-4">
           <svg
             class="h-5 w-5"
@@ -45,172 +121,7 @@ function portfolio() {
             Quaerat, saepe?
           </p>
         </div>
-      </div>
-
-      <div class="flex items-start gap-4">
-        <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            class="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
-
-        <div>
-          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-          <p class="mt-1 text-sm text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-            cumque tempore est ab possimus quisquam reiciendis tempora animi!
-            Quaerat, saepe?
-          </p>
-        </div>
-      </div>
-
-      <div class="flex items-start gap-4">
-        <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            class="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
-
-        <div>
-          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-          <p class="mt-1 text-sm text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-            cumque tempore est ab possimus quisquam reiciendis tempora animi!
-            Quaerat, saepe?
-          </p>
-        </div>
-      </div>
-
-      <div class="flex items-start gap-4">
-        <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            class="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
-
-        <div>
-          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-          <p class="mt-1 text-sm text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-            cumque tempore est ab possimus quisquam reiciendis tempora animi!
-            Quaerat, saepe?
-          </p>
-        </div>
-      </div>
-
-      <div class="flex items-start gap-4">
-        <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            class="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
-
-        <div>
-          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-          <p class="mt-1 text-sm text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-            cumque tempore est ab possimus quisquam reiciendis tempora animi!
-            Quaerat, saepe?
-          </p>
-        </div>
-      </div>
-
-      <div class="flex items-start gap-4">
-        <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-          <svg
-            class="h-5 w-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-            <path
-              d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-            ></path>
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-            ></path>
-          </svg>
-        </span>
-
-        <div>
-          <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-          <p class="mt-1 text-sm text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-            cumque tempore est ab possimus quisquam reiciendis tempora animi!
-            Quaerat, saepe?
-          </p>
-        </div>
-      </div>
+      </div> */}
     </div>
   </div>
 </section>
